e26be7f55f3ed149509931521c262af1c28bdf85
Jake Smith <jacob.smith2@clcs.conduent.com>
2019-07-31 16:59:01 -0400

    Fixed pylint errors in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
+#!/usr/bin/env python3
+"""Script to create executable of game"""
+
 import cx_Freeze
 
-executables = [cx_Freeze.Executable("pong.py")]
+EXECUTABLES = [cx_Freeze.Executable("pong.py")]
 
 cx_Freeze.setup(
   name="Pong",
-  options={"build_exe": {"packages": ["pygame"], "include_files": ["sounds/pong_bounce.ogg", "sounds/pong_victory.ogg"]}},
-  executables=executables
+  options={
+    "build_exe": {
+      "packages": ["pygame"],
+      "include_files": [
+        "sounds/pong_bounce.ogg",
+        "sounds/pong_victory.ogg"
+      ]
+    }
+  },
+  EXECUTABLES=EXECUTABLES
 )
